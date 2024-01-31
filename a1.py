36bd66c6a6aaf2e84b4e950e72dd880b9c536598
julien <juliensavary2001@gmail.com>
2024-01-31 10:58:21 -0800

    cleaning

--- a1.py
+++ a1.py
@@ -2,7 +2,6 @@
 # 22982687
 # Project 1: File Escavator
 
-from sys import exit
 from pathlib import Path
 
 
@@ -10,25 +9,21 @@ def catch_it(pass_it: str):
     catch = pass_it.split()
     first_command = catch[0]
     path = catch[1]
-    options = catch[2:-1]
-    search = catch[-1]
-
-    print(options)
-
-def quit_():
-    exit(0)
+    flags = catch[2:-1]
+    choice = catch[-1]
 
 
 def main():
-    try:
-        pass_it = input()
-        catch_it(pass_it)
-    except KeyboardInterrupt:
-        print("\nProgram interrupted by user.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-    finally:
-        pass
+    while pass_it != 'Q':
+        try:
+            pass_it = input()
+            catch_it(pass_it)
+        except KeyboardInterrupt:
+            print("\nProgram interrupted by user.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+        finally:
+            pass
 
 
 if __name__ == "__main__":
